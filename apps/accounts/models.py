21844a6c04fda07fdcdfe9e035f1a654a72e2c2d
Bibhas <iambibhas@gmail.com>
2014-10-17 19:15:10 +0530

    added half baked app for board membership, moved apps to apps/

---
+++ apps/accounts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# class User(AbstractUser):
+
+
+#     class Meta(AbstractUser.Meta):
+#         swappable = 'AUTH_USER_MODEL'
---
+++ apps/board/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+class Designation(models.Model):
+    """
+    Posibile board positions.
+    Can be added or deleted according to board decision.
+    """
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class BoardMember(models.Model):
+    """
+    The board members. Each have a start date and end date
+    """
+    user = models.ForeignKey(get_user_model())
+    designation = models.ForeignKey('Designation')
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField(black=True, null=True)
+
+    def __unicode__(self):
+        return "{designation}: {user_name}".format(
+            designation=self.designation,
+            user_name=self.user.get_full_name()
+        )
+
+
+class Election(models.Model):
+    """
+    Election for board member positions
+    """
+    designation = models.ForeignKey('Designation')
+    candidates = models.ManyToManyField(get_user_model(),
+        related_name='elections_nominated')
+    winner = models.ForeignKey(get_user_model(), black=True, null=True)
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField(black=True, null=True)
+
+    def __unicode__(self):
+        return '{designation}: {date}'.format(
+            designation=self.designation,
+            date=self.start_date
+        )
+
+
+class Vote(models.Model):
+    """
+    The vote entries. These should not be visible in the admin panel.
+    """
+    election = models.ForeignKey('Election')
+    user = models.ForeignKey(get_user_model())
+    candidate = models.ForeignKey(get_user_model())
+    created_at = models.DateTimeField(auto_now_add=True)
+
