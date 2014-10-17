3ce1a5a39b6cd6f1e9f97c43a77c5d9a6b56d5f4
Bibhas <iambibhas@gmail.com>
2014-10-17 23:32:45 +0530

    created base model under common app, commented election for now

--- apps/board/models.py
+++ apps/board/models.py
@@ -1,27 +1,31 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+from django.conf import settings
+from common.models import BaseModel
 
 
-class Designation(models.Model):
+class Designation(BaseModel):
     """
     Posibile board positions.
     Can be added or deleted according to board decision.
     """
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
         return self.name
 
 
-class BoardMember(models.Model):
+class BoardMember(BaseModel):
     """
     The board members. Each have a start date and end date
     """
-    user = models.ForeignKey(get_user_model())
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
     designation = models.ForeignKey('Designation')
     start_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField(black=True, null=True)
+
+    class Meta:
+        unique_together = ('user', 'designation', 'start_date', 'end_date')
 
     def __unicode__(self):
         return "{designation}: {user_name}".format(
@@ -30,30 +34,30 @@ class BoardMember(models.Model):
         )
 
 
-class Election(models.Model):
-    """
-    Election for board member positions
-    """
-    designation = models.ForeignKey('Designation')
-    candidates = models.ManyToManyField(get_user_model(),
-        related_name='elections_nominated')
-    winner = models.ForeignKey(get_user_model(), black=True, null=True)
-    start_date = models.DateTimeField(auto_now_add=True)
-    end_date = models.DateTimeField(black=True, null=True)
+# class Election(BaseModel):
+#     """
+#     Election for board member positions
+#     """
+#     designation = models.ForeignKey('Designation')
+#     candidates = models.ManyToManyField(settings.AUTH_USER_MODEL,
+#         related_name='elections_nominated')
+#     winner = models.ForeignKey(settings.AUTH_USER_MODEL, black=True, null=True)
+#     start_date = models.DateTimeField(auto_now_add=True)
+#     end_date = models.DateTimeField(black=True, null=True)
 
-    def __unicode__(self):
-        return '{designation}: {date}'.format(
-            designation=self.designation,
-            date=self.start_date
-        )
+#     def __unicode__(self):
+#         return '{designation}: {date}'.format(
+#             designation=self.designation,
+#             date=self.start_date
+#         )
 
 
-class Vote(models.Model):
-    """
-    The vote entries. These should not be visible in the admin panel.
-    """
-    election = models.ForeignKey('Election')
-    user = models.ForeignKey(get_user_model())
-    candidate = models.ForeignKey(get_user_model())
-    created_at = models.DateTimeField(auto_now_add=True)
+# class Vote(BaseModel):
+#     """
+#     The vote entries. These should not be visible in the admin panel.
+#     """
+#     election = models.ForeignKey('Election')
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL)
+#     candidate = models.ForeignKey(settings.AUTH_USER_MODEL)
+#     created_at = models.DateTimeField(auto_now_add=True)
 
