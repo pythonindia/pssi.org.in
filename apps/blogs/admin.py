cc86331c838ad5fde16c94de60f3b47dd7a0b8d4
Bibhas <iambibhas@gmail.com>
2014-10-25 03:19:43 +0530

    added comment

--- apps/blogs/admin.py
+++ apps/blogs/admin.py
@@ -3,6 +3,8 @@ from .models import Post, Tag
 
 
 class PostAdmin(admin.ModelAdmin):
+    # slug should be prepopulated from the title
     prepopulated_field = {'slug': ('title', )}
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
