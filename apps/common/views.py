ad7f6fc83332ae6a60432c0c726350e4a618ac8b
Bibhas <iambibhas@gmail.com>
2014-11-12 03:45:14 +0530

    added csrf exempt mixin

--- apps/common/views.py
+++ apps/common/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 
 
 class LoginRequiredMixin(object):
@@ -6,3 +7,10 @@ class LoginRequiredMixin(object):
     def as_view(cls, **initkwargs):
         view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
         return login_required(view)
+
+
+class CSRFExemptMixin(object):
+    @classmethod
+    def as_view(cls, **initkwargs):
+        view = super(CSRFExemptMixin, cls).as_view(**initkwargs)
+        return csrf_exempt(view)
