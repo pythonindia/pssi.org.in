a43820d7ac942037afbf270fc524b2788f4d7fc0
Bibhas <iambibhas@gmail.com>
2014-11-14 01:49:03 +0530

    added membership and payment admins

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import UserProfile, Membership
 
 
-admin.site.register([UserProfile, Membership])
+class MembershipAdmin(admin.ModelAdmin):
+    model = Membership
+    list_display = ('get_username', 'created_at', 'from_date', 'to_date')
+
+    def get_username(self, obj):
+        return obj.profile.user.username
+    get_username.short_description = 'User'
+    get_username.admin_order_field = 'profile__user__username'
+
+
+admin.site.register(Membership, MembershipAdmin)
+admin.site.register([UserProfile])
--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import PaymentGateway, PaymentType, Payment
 
-admin.site.register([PaymentGateway, PaymentType, Payment])
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('payment_id', 'ptype', 'created_at', 'gateway', 'amount')
+    search_fields = ('payment_id',)
+
+
+admin.site.register(Payment, PaymentAdmin)
+admin.site.register([PaymentGateway, PaymentType])
