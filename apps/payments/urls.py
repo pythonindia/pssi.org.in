f5c652d06c7c4e5759c35a96ca53ad8f581d6c23
Bibhas <iambibhas@gmail.com>
2014-11-15 13:21:31 +0530

    Revert "Added user profile and payment models"

--- apps/payments/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from .views import (MembershipPaymentConfirmView)
-
-urlpatterns = patterns(
-    '',
-    url(r'^confirm/membership/$', MembershipPaymentConfirmView.as_view(), name='payment_membership_confirm'),
-)
--- pssi/settings.py
+++ pssi/settings.py
@@ -29,6 +29,9 @@ ADMINS = (
     ('Kracekumar', 'me@kracekumar.com')
 )
 
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 # Application definition
 
 INSTALLED_APPS = (
@@ -49,11 +52,9 @@ INSTALLED_APPS = (
     'django_markdown',
     'bootstrap3',
 
-    'accounts',
     'board',
     'grants',
     'blogs',
-    'payments',
 )
 
 MIDDLEWARE_CLASSES = (
