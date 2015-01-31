7a5fd2e56976204326e04df18303e5907b6f74c6
vijay <vijay@indee.tv>
2015-01-31 10:21:09 +0530

    Added space as per pep8
    Added space as per pep8

--- apps/board/admin.py
+++ apps/board/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import Designation,BoardMember
+from .models import Designation, BoardMember
 # Register your models here.
-
 admin.site.register(Designation)
 admin.site.register(BoardMember)
