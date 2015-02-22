d3b051404d200fffb47c4ac2f9a6a3693ba1e9cf
Kracekumar Ramaraju <me@kracekumar.com>
2015-02-22 23:17:50 +0530

    Update service.py

--- apps/common/service.py
+++ apps/common/service.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from django.contrib.auth.models import User
+from django.conf import settings
 
 
 def get_all_staff_emails():
     """Return all staff emails
     """
-    return User.objects.filter(is_staff=True).values_list('email', flat=True)
+    return settings.STAFF_EMAILS
